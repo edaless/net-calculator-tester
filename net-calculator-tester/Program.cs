967397c2622de5a95426fe3d3393df261ce10587
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2023-04-26 15:40:56 +0200

    4 funzioni calcolatrice

--- net-calculator-tester/Program.cs
+++ net-calculator-tester/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            float Add(float num1, float num2)
+            {
+                return num1 + num2;
+            }
+            float Subtract(float num1, float num2)
+            {
+                return num1 - num2;
+            }
+            float Divide(float num1, float num2)
+            {
+                return num1 / num2;
+            }
+            float Multiply(float num1, float num2)
+            {
+                return num1 * num2;
+            }
+
+
+            Console.WriteLine(Add(2, 5));
+            Console.WriteLine(Subtract(2, 5));
+            Console.WriteLine(Divide(2, 5));
+            Console.WriteLine(Multiply(2, 5));
+
+
         }
+
     }
 }
